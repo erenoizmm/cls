ecec3de28614cf599c347c5f4be7d9d9822b9f7a
erenoizmm <100294278+erenoizmm@users.noreply.github.com>
2024-11-30 00:38:38 +0300

    Update layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next"
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Script from 'next/script'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'BONUS KRALI',
+  description: 'Deneme Bonusu',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+        {children}
+      </body>
+    </html>
+  )
+}
