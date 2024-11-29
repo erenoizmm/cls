f3ac7bcfab4865645f67df75ba45093d63425ed2
lyrycx <xpml@gmx.us>
2024-11-29 21:22:37 +0300

    Initial commit: TON Telegram Mini App with MongoDB and Prisma

---
+++ app/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['r.resimlink.com'],
+      unoptimized: true
+    }
+  }
+  
+  module.exports = nextConfig
